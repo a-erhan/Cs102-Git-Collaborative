055fbe6c4ed9464c45291dfe9edb6d0cb6e6b667
Elvin <ismayilelvin1@gmail.com>
2026-01-28 16:13:34 +0300

    Implement findTheSum method for array summation
    Add findTheSum method to calculate sum of odd or even indexed elements

--- Main.java
+++ Main.java
@@ -38,4 +38,23 @@ public class Main {
             }
             return new int[]{min, max};
         }
+
+     public static int findTheSum(int[] arr, boolean isOdd){
+
+            int sum = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if(isOdd){
+                    if(i%2 != 0){
+                        sum += arr[i];
+                    } 
+                }
+                else{
+                    if(i%2 == 0){
+                        sum += arr[i];
+                    }
+                }
+            }
+            return sum;
+        }
+
 }
